1aeacc293116d9f540b96fb04b7336ed73da414b
oreldm <orel@mip.me>
2018-04-04 20:12:09 +0300

    adding dip to pixels

--- FirstApplication/app/src/main/java/com/example/h1z1/firstapplication/MainActivity.java
+++ FirstApplication/app/src/main/java/com/example/h1z1/firstapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.h1z1.firstapplication;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Button;
 import android.graphics.Color;
@@ -15,10 +18,18 @@ public class MainActivity extends AppCompatActivity {
         RelativeLayout layout=new RelativeLayout(this);
         layout.setBackgroundColor(Color.GREEN);
 
+        Resources r  = getResources(); //to get data about the application
+
         RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.WRAP_CONTENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT
         );
+
+        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
 
         buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
         buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
@@ -26,8 +37,24 @@ public class MainActivity extends AppCompatActivity {
         Button redButton = new Button(this);
         redButton.setText("Click Me!");
         redButton.setBackgroundColor(Color.RED);
+        redButton.setId(1);
+
+        //Username input
+        EditText username = new EditText(this);
+        username.setId(2);
+
+        int usernamePixelsWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 200 ,
+                r.getDisplayMetrics()); //dip value to pixels value
+
+        username.setWidth(usernamePixelsWidth);
+
+        //give rules to position widgets
+        usernameDetails.addRule(RelativeLayout.ABOVE, redButton.getId());
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.setMargins(0,0,0,50);
 
         layout.addView(redButton, buttonDetails);
+        layout.addView(username,usernameDetails);
         setContentView(layout);
 
     }
