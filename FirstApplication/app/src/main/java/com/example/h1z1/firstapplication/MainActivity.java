8ff39448ccafbd897f231c230af55d6654a16d38
oreldm <orel@mip.me>
2018-04-04 19:52:55 +0300

    test

---
+++ FirstApplication/app/src/main/java/com/example/h1z1/firstapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.h1z1.firstapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.graphics.Color;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        RelativeLayout layout=new RelativeLayout(this);
+        layout.setBackgroundColor(Color.GREEN);
+
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        ); 
+
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        Button redButton = new Button(this);
+        redButton.setText("Click Me");
+        redButton.setBackgroundColor(Color.RED);
+
+        layout.addView(redButton, buttonDetails);
+        setContentView(layout);
+
+    }
+
+}
